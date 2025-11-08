e92144b05faf72f0f61546d2404049efd4cfdf4b
almamuntmn <almamuncool88@gmail.com>
2025-11-08 13:27:12 +0600

    login page

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { Link } from 'react-router';
+import { auth } from '../../../firebase-init';
 
 const Login = () => {
+
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    // Handle login logic here
+const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+
+    setError('');
+    setSuccess(false);
+
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then(result => {
+        const user = result.user;
+        console.log(user);
+        setSuccess(true);
+    })
+    .catch(error => {
+        console.error(error);
+        setError(error.message);
+    })
+}
+
+
     return (
-        <div>
-            <h2>This is Login</h2>
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <h1 className="text-3xl font-bold my-4 text-center">Login now!</h1>
+                <div className="card card-body">
+                    <form onSubmit={handleLogin} className='space-y-2'>
+                        <label className="label">Email</label>
+                        <input type="email" className="input" name='email' placeholder="Email" required/>
+                        <label className="label">Password</label>
+                        <input type="password" className="input" name='password' placeholder="Password" required autoComplete='password'/>
+                        <div><a className="link link-hover">Forgot password?</a></div>
+                        <br />
+                        <input className='btn btn-primary' type="submit" value="Login" />
+                        <p>Don't have an account? <Link className='link link-hover text-blue-500' to='/signup'>Register</Link></p>
+                        {error && <p className='text-red-500'>{error}</p>}
+                        {success && <p className='text-green-500'>Login successful!</p>}
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/component/Singup/Signup.jsx
+++ src/component/Singup/Signup.jsx
@@ -1,41 +1,48 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react';
 import { auth } from '../../../firebase-init';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { Link } from 'react-router';
 
 const Signup = () => {
     const [error, setError] = useState(false);
     const [success, setSuccess] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
-const handleLogin = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const email = form.email.value;
-    const password = form.password.value;
-    console.log(email, password);
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const terms = form.terms.checked;
+        console.log(email, password, terms);
 
-    setError('');
-    setSuccess(false);
+        setError('');
+        setSuccess(false);
 
-    // Password validation
-   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-   if (!passwordRegex.test(password)) {
-    setError('Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.');
-    return;
-   }
+        if (!terms) {
+            setError('Please accept the terms and conditions.');
+            return;
+        }
 
-    createUserWithEmailAndPassword(auth, email, password)
-    .then(result => {
-        const user = result.user;
-        console.log(user);
-        setSuccess(true); 
-    })
-    .catch(error => {
-        console.error(error);
-        setError(error.message);
-    })
-}
+        // Password validation
+        const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+        if (!passwordRegex.test(password)) {
+            setError('Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.');
+            return;
+        }
 
-
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setSuccess(true);
+            })
+            .catch(error => {
+                console.error(error);
+                setError(error.message);
+            })
+    }
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -45,9 +52,26 @@ const handleLogin = (e) => {
                             <label className="label">Email</label>
                             <input type="email" className="input" name='email' placeholder="Email" />
                             <label className="label">Password</label>
-                            <input type="password" className="input" name='password' placeholder="Password" />
+                            <div className='relative'>
+                                <input
+                                    type={showPassword ? 'text' : 'password'}
+                                    className="input"
+                                    name='password' placeholder="Password" />
+                                <div onClick={() => setShowPassword(!showPassword)} className='absolute top-1/2 right-2 transform -translate-y-1/2'>
+                                    {
+                                        showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                                    }
+                                </div>
+                            </div>
                             <div><a className="link link-hover">Forgot password?</a></div>
-                            <input className='btn btn-primary' type="submit" value="Login" />
+                            <label className="label">
+                                <input type="checkbox" name="terms" className="checkbox" />
+                                Accept our terms and conditions
+                            </label>
+                            <br />
+                            <input className='btn btn-primary' type="submit" value="Signup" />
+                            <br />
+                            <p>Already have an account? <Link to='/login' className='link link-hover text-blue-400'>Login</Link></p>
                         </form>
                         {
                             error && <p className='text-red-600'>{error}</p>
